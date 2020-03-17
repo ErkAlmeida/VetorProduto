b0c1d8f54fe8d474022a266d7a7ed0a55e707b16
Erick Almeida <erickprofissional@gmail.com>
2020-03-16 23:08:01 -0300

    Finalizado o calculo medio

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -2,7 +2,7 @@
 Fazer um programa para ler um número inteiro N e os dados 
 (nome e preço) de N Produtos. 
 Armazene os N produtos em um vetor. 
-Em seguida,mostraropreçomédiodosprodutos.
+Em seguida,mostrar o preço médio dos produtos.
 */
 package aplicacao;
 
@@ -27,13 +27,22 @@ public class Programa {
 		
 		for(int i=0; i< n; i++)
 		{
+			sc.nextLine();
+			System.out.println("Nome: ");
 			String nome  = sc.nextLine();
+			System.out.print("Preço: ");
 			double preco = sc.nextDouble();
 			
 			vetor[i] = new Produto(nome, preco);
  		}
 		
-		System.out.print(n);
+		double soma = 0;
+		for(int i = 0; i < n;i++) 
+		{
+			soma += vetor[i].getPreco();
+		}
+		
+		System.out.printf("O Preço medio dos produtos:" + (soma)/n);
 		
 	
 		sc.close();
--- src/entidade/Produto.java
+++ src/entidade/Produto.java
@@ -14,5 +14,21 @@ public class Produto
 		this.nome = nome;
 		this.preco = preco;
 	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
 }
 
